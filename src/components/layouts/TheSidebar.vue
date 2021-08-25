a914a358b0267e8a97186f77096728b52035c57f
Seto Kuslaksono <kuslaksono@gmail.com>
2021-08-25 21:08:12 +0700

    Add link to login page

--- src/components/layouts/TheSidebar.vue
+++ src/components/layouts/TheSidebar.vue
@@ -208,6 +208,28 @@
             Calendar
           </a>
         </li>
+
+        <li class="px-4 py-2 mt-2 text-xs uppercase tracking-wider text-gray-500 font-bold">Pages</li>
+
+        <li class="px-4 cursor-pointer hover:bg-gray-700">
+          <router-link :to="{ name: 'login' }" class="py-2 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+              />
+            </svg>
+            Login
+          </router-link>
+        </li>
       </ul>
     </nav>
   </aside>
