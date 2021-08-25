c30e74853e1bb99b80b9fffbbf48e71265645402
Seto Kuslaksono <kuslaksono@gmail.com>
2021-08-25 21:27:00 +0700

    Increase footer padding to match overall layout

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex justify-between mt-4 px-6 py-4 text-gray-500 border-t-2 text-xs font-bold bg-white">
+  <footer class="flex justify-between mt-4 px-8 py-4 text-gray-500 border-t-2 text-xs font-bold bg-white">
     <div class="plex space-x-4">
       <a href="#" class="hover:text-indigo-600">User Guide</a>
       <a href="#" class="font-bold hover:text-indigo-600">FAQ</a>
