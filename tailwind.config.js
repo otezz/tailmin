0cd51835a95f822d19f4ff64c961efb50f681bb5
Seto Kuslaksono <kuslaksono@gmail.com>
2021-09-02 11:37:33 +0700

    Add more class to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    safelist: ['bg-blue-500', 'bg-yellow-500'],
+    safelist: ['bg-red-500', 'bg-green-500', 'bg-blue-500', 'bg-yellow-500'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
